613d614a110eb243dde6ccee78447d1acacd22ee
harsh_rana_dev <hr2322808@gmail.com>
2026-08-16 22:59:57 +0530

    lets go_2

---
+++ for_claude.py
@@ -0,0 +1,5 @@
+ok now i have writen the codes by myself and i used help where i needed even then i have writen about 50% to 60% of the code by myself.
+
+so now review the codes and related question and insites.
+
+def
