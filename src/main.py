2bd73f7f23c76008d589c2c0b527b080505ae4fb
harsh_rana_dev <hr2322808@gmail.com>
2026-08-21 00:00:16 +0530

    lets go pydantic

--- src/main.py
+++
@@ -1,8 +0,0 @@
-def count_word_frequency(words: list[str]) -> dict[str, int]:
-    frequency = {}
-    for item in words:
-        frequency[item] = frequency.get(item, 0) + 1 
-    return frequency
-
-def reverse_string(text: str) -> str:
-    return text[::-1]
