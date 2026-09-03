6869011e8e40cc28567ffce602917fb3b0dd1759
harsh_rana_dev <hr2322808@gmail.com>
2026-09-04 00:00:36 +0530

    done

--- prep_1/day_2.sql
+++ prep_1/day_2.sql
@@ -1,57 +1,53 @@
-ok so the thing is that for these sql questions I needed hints I got the hints from Gemini
-now review the codes and then I will rewrite it without hints 
-
 orders(order_id, customer_id, order_date, amount, status)
 customers(customer_id, name, country, signup_date)
 
-SELECT 
-    order_id, 
-    customer_id,
-    order_date, 
-    amount,
-    LAG(amount) OVER (
-        PARTITION BY customer_id 
-        ORDER BY order_date
-    ) AS previous_order_amount
-FROM orders;
 
-SELECT 
-    c.name, 
+select 
+    order_id,
+    customer_id,
+    order_date,
+    amount,
+    lag(amount) over (partition by customer_id order by order_date) as previous_order_amount #
+from orders;
+
+select
+    c.name,
     sum(o.amount) as total_amount,
-    dense_rank() over (order by sum(o.amount) desc) as customer_rank
+    dense_rank() over (order by sum(o.amount) desc) as customer_rank #
 from customers c 
 join orders o on c.customer_id = o.customer_id
-group by c.name;
+group by c.name; #
 
-SELECT
+select
     order_id,
     customer_id,
     amount,
-    amount - avg(amount) over (PARTITION by customer_id) as diff_avg
+    amount - avg(amount) over (partition by customer_id) as deff_avg #
 from orders;
 
 with customer_table as (
-    SELECT
+    select
         customer_id,
         sum(amount) as total_amount
     from orders
-    group by customer_id
+    group by customer_id #
 )
-SELECT customer_id, total_amount
+select customer_id, total_amount
 from customer_table
-where total_amount > (SELECT avg(total_amount) from customer_table);
+where total_amount > (select avg(total_amount) from customer_table); # 
 
-WITH ranked_orders AS (
-    SELECT
+with ranked_ordars as (
+    select
         customer_id,
         order_id,
         amount,
-        DENSE_RANK() OVER (
-            PARTITION BY customer_id 
-            ORDER BY amount DESC
-        ) AS rnk 
-    FROM orders 
+        dense_rank() over (partition by customer_id
+        order by amount desc) as rnk #
+    from orders 
 )
-SELECT customer_id, order_id, amount
-FROM ranked_orders
-WHERE rnk = 2;
+select 
+    customer_id,
+    order_id,
+    amount
+from ranked_ordars
+where rnk = 2; #
