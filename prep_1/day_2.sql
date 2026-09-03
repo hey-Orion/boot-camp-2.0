6b09003ba1d6e51754300a7529cf3784a1aee115
Harsh Rana <harshranaorion@gmail.com>
2026-09-03 19:57:07 +0530

    Update day_2.sql

--- prep_1/day_2.sql
+++ prep_1/day_2.sql
@@ -1,3 +1,6 @@
+ok so the thing is that for these sql questions I needed hints I got the hints from Gemini
+now review the codes and then I'll rewrite it without hints 
+
 orders(order_id, customer_id, order_date, amount, status)
 customers(customer_id, name, country, signup_date)
 
