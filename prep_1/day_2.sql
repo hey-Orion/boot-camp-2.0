c0ae53585a1b3f45f2cb1035d56c00aeeb141b3b
harsh_rana_dev <hr2322808@gmail.com>
2026-09-03 18:07:48 +0530

    brake

--- prep_1/day_2.sql
+++ prep_1/day_2.sql
@@ -1,3 +1,14 @@
 orders(order_id, customer_id, order_date, amount, status)
 customers(customer_id, name, country, signup_date)
 
+SELECT 
+    order_id, 
+    customer_id,
+    order_date, 
+    amount,
+    LAG(amount) OVER (
+        PARTITION BY customer_id 
+        ORDER BY order_date
+    ) AS previous_order_amount
+FROM orders;
+
