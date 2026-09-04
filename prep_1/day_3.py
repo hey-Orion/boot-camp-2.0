498f6078fc9483ecb6d7b4ac6877df3ecd903b02
harsh_rana_dev <hr2322808@gmail.com>
2026-09-05 01:02:27 +0530

    done

--- prep_1/day_3.py
+++ prep_1/day_3.py
@@ -3,3 +3,14 @@ customers_df: [customer_id, name, country, signup_date]
 import pandas as pd 
 import numpy as np
 
+filterd_df = orders_df[(orders_df['status'] == 'completed') & (orders_df['amount'] > 100)]
+
+customer_summary = orders_df.groupby('customer_id')['amount'].agg(['sum', 'count']).reset_index()
+
+merged_df = orders_df.merge(customer_df, on='customer_id')
+germany_orders = merged_df[merged_df['country'] == 'Germany']
+
+avg_df = orders_df.groupby('cuctomer_id')['amount'].mean().reset_index()
+filterd_customers = avg_df[['amount'] > 200]
+
+orders_df['value_flags'] = np.where(orders_df['amount'] > 500, 'heigh_value', 'normal')
--- prep_1/day_4.py
+++ prep_1/day_4.py
@@ -1,12 +1,1 @@
-filterd_df = orders_df[(orders_df['status'] == 'completed') & (orders_df['amount'] > 100)]
-
-customer_summary = orders_df.groupby('customer_id')['amount'].agg(['sum', 'count']).reset_index()
-
-merged_df = orders_df.merge(customers_df, on='customer_id')
-germany_orders = merged_df[merged_df['country'] == 'Germany']
-
-avg_df = orders_df.groupby('customer_id')['amount'].mean().reset_index() what is reset_index
-filterd_customers = avg_df[avg_df['amount'] > 200]
-
-
 orders_df['value_flags'] = np.where(orders_df['amount'] > 500, 'high_value', 'normal')
